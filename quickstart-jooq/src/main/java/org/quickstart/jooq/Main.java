eec1204e9c2733f1e9de6a3526c0f91c440054f5
yangzl <youngzil@163.com>
2018-11-11 23:17:26 +0800

    新增jooq项目

---
+++ quickstart-jooq/src/main/java/org/quickstart/jooq/Main.java
@@ -0,0 +1,55 @@
+/**
+ * 项目名称：quickstart-jooq 
+ * 文件名：Main.java
+ * 版本信息：
+ * 日期：2018年11月11日
+ * Copyright asiainfo Corporation 2018
+ * 版权所有 *
+ */
+package org.quickstart.jooq;
+
+import static org.quickstart.jooq.Tables.AUTHOR;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+
+/**
+ * Main
+ * 
+ * @author：yangzl@asiainfo.com
+ * @2018年11月11日 下午10:54:17
+ * @since 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        String userName = "root";
+        String password = "root";
+        String url = "jdbc:mysql://10.11.20.83:3306/library";
+
+        // Connection is the only JDBC resource that we need
+        // PreparedStatement and ResultSet are handled by jOOQ, internally
+        try (Connection conn = DriverManager.getConnection(url, userName, password)) {
+            DSLContext create = DSL.using(conn, SQLDialect.MYSQL);
+            Result<Record> result = create.select().from(AUTHOR).fetch();
+
+            for (Record r : result) {
+                Integer id = r.getValue(AUTHOR.ID);
+                String firstName = r.getValue(AUTHOR.FIRST_NAME);
+                String lastName = r.getValue(AUTHOR.LAST_NAME);
+
+                System.out.println("ID: " + id + " first name: " + firstName + " last name: " + lastName);
+            }
+        }
+
+        // For the sake of this tutorial, let's keep exception handling simple
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
