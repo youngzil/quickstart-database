291206bcf98c7c275d1fbd5dd4d22693c5aa2aa3
yangzl <youngzil@163.com>
2019-08-25 09:35:59 +0800

    satagrip的sql脚本

---
+++ quickstart-datagrip/docs/杭州业务大厅网关.sql
@@ -0,0 +1,43 @@
+INSERT INTO ""("ABILITY_ID", "ABILITY_NAME", "ABILITY_CODE", "ABILITY_SKETCH", "ABILITY_SORT", "ABILITY_URL", "ABILITY_LOGO", "QUOTA_NUMS", "FLOW_NUMS", "ESB_DOMAIN_ID", "ABILITY_FORMAT", "ABILITY_STATUS", "REMARKS", "CREATOR", "CREATE_TIME", "UPDATOR", "UPDATE_TIME", "CYCLE_TYPE", "QUOTA_TYPE", "ABILITY_TYPE", "ABILITY_SHORT_ADDR") VALUES ('100000000408', 'hallOpenGsm_ICertCheckCSV_queryCertTypeList', 'hallOpenGsm_ICertCheckCSV_queryCertTypeList', NULL, '1', 'http://20.26.37.180:20150/rest/hallOpenGsm_ICertCheckCSV_queryCertTypeList', '1', NULL, NULL, '2,3', 'json', '3', NULL, '2000001', TO_DATE('2019-05-06 11:17:21', 'SYYYY-MM-DD HH24:MI:SS'), NULL, NULL, NULL, NULL, '1', NULL);
+
+select count(*) from AOP_ABILITY_BASEINFO where ABILITY_CODE='hallOpenGsm_IBusiSceneCSV_qryBusiScene'
+
+select * from AOP_ABILITY_BASEINFO where ABILITY_ID in (100000000808,100000000408)
+
+delete from AOP_ABILITY_BASEINFO where ABILITY_ID in (100000000408)
+
+
+select * from AOP_ABILITY_BASEINFO where ABILITY_ID>=100000000800 and ABILITY_ID<=100000000809
+select * from AOP_ABILITY_PROTOCOL where ABILITY_ID>=100000000800 and ABILITY_ID<=100000000809
+
+select * from AOP_ABILITY_BASEINFO
+
+dbpath=/app/memdb/mongodb/data/node3/conf
+
+logpath=/app/memdb/mongodb/log/node3/mongo_config.log
+
+
+mongod --repair --dbpath /app/memdb/mongodb/data/node3/conf / --repairpath /app/memdb/mongodb/log/node3/mongo_config.log
+
+select * from AOP_ABILITY_BASEINFO where ABILITY_ID in (100000000800,100000000408)
+
+
+select * from AOP_ABILITY_BASEINFO where ABILITY_url like 'http://aifgatewaytest.zj.chinamobile.com%' or ABILITY_ID in (100000000808,100000000408)
+
+
+select * from AOP_ABILITY_PROTOCOL where CALL_PROTOCOL='7' and ABILITY_ID in (100000000800,100000000801,100000000802,100000000803,100000000804,100000000805,100000000806,100000000807,100000000808,100000000809)
+
+
+
+select * from AOP_ABILITY_EXTINFO where ability_id=100000000801
+
+select * from AOP_ABILITY_EXTINFO where ABILITY_ID in (100000000808,100000000408)
+
+
+select * from AOP_ABILITY_BASEINFO where ABILITY_ID>=100000000800 and ABILITY_ID<=100000000809
+
+
+-- 报文大小限制修改
+select * from ospgateway.AOP_PARA_DETAIL where PARA_TYPE='AOP_REQUEST';
+update  ospgateway.AOP_PARA_DETAIL set PARA1='115857600' where PARA_TYPE='AOP_REQUEST' and PARA_CODE='MAX_LENGTH';
+
